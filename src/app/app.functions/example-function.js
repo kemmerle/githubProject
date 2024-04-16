37d5a592fddf771c573da6f59f429e6a16503922
Allison Kemmerle <akemmerle@hubspot.com>
2024-04-16 12:46:13 -0400

    Third commit

--- src/app/app.functions/example-function.js
+++ src/app/app.functions/example-function.js
@@ -1,7 +1,7 @@
 exports.main = async (context = {}) => {
   const { text } = context.parameters;
 
-  const response = `This is coming from a serverless function! I'm changing this up for testing purposes. You entered: ${text}`;
+  const response = `This is coming from a serverless function! I'm changing this up for testing purposes 333. You entered: ${text}`;
 
   return response;
 };
